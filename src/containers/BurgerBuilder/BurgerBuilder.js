f2acbffae2327214c0a928b7c399b400993906bd
Mario <mjvinti@gmail.com>
2020-02-26 22:11:42 -0500

    finished reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,13 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
-    axios.get('https://react-burger-builder-73850.firebaseio.com/ingredients.json')
-      .then(res => {
-        this.setState({ ingredients: res.data });
-      })
-      .catch(err => {
-        this.setState({ error: true });
-      });
+    // axios.get('https://react-burger-builder-73850.firebaseio.com/ingredients.json')
+    //   .then(res => {
+    //     this.setState({ ingredients: res.data });
+    //   })
+    //   .catch(err => {
+    //     this.setState({ error: true });
+    //   });
   }
 
   updatePurchaseState = (ingredients) => {
