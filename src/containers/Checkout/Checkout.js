64c3f86482fe4c47943a33be4549d2d331e6d92a
Mario <mjvinti@gmail.com>
2020-03-17 21:55:05 -0400

    handling purchases and updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -17,15 +17,17 @@ class Checkout extends Component {
   render() {
     let summary = <Redirect to="/" />;
     if (this.props.ings) {
+      const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
       summary = (
         <div>
+          {purchasedRedirect}
           <CheckoutSummary
             ingredients={this.props.ings}
             checkoutCancelled={this.checkoutCancelledHandler}
             checkoutContinued={this.checkoutContinuedHandler}
           />
           <Route
-            path={this.props.match.path + "/contact-data"}
+            path={this.props.match.path + '/contact-data'}
             component={ContactData}
           />
         </div>
@@ -37,7 +39,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
   return {
-    ings: state.burgerBuilder.ingredients
+    ings: state.burgerBuilder.ingredients,
+    purchased: state.order.purchased
   };
 };
 
