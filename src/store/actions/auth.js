2fec30d6414d5eecbdadb317fbdefb92d2d22d59
Mario <mjvinti@gmail.com>
2020-03-24 20:38:35 -0400

    logging users out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,20 @@ export const authFail = (error) => {
   };
 };
 
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  }; 
+};
+
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
     dispatch(authStart());
@@ -42,6 +56,7 @@ export const auth = (email, password, isSignUp) => {
       .then(res => {
         console.log(res);
         dispatch(authSuccess(res.data.idToken, res.data.localId));
+        dispatch(checkAuthTimeout(res.data.expiresIn));
       })
       .catch(err => {
         console.log(err);
